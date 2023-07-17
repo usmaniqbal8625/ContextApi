9bb4a25d9f419b55085725a00d490aeb4b7cf1c7
usmaniqbal8625 <usmaniqbal8625@gmail.com>
2023-07-17 13:09:06 +0500

    contextapi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Home } from './context/Home';
+import About from './context/About';
+
+import NoteState from './context/noteState';
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NoteState>
+        <BrowserRouter>
+       
+       <Routes>
+     <Route path="/" element={<Home/>} />
+    <Route path="/home" element={<Home/>} /> */}
+    
+    <Route path="/about" element={<About/>} />
+    
+    
+    
+       </Routes>
+       </BrowserRouter>
+       </NoteState>
   );
 }
 
